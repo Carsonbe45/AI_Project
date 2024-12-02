ec2362d9d907997fa06157806b2a7a795e0dc5e7
jimmymachine <jimmyhranowskyj@gmail.com>
2024-12-02 17:18:11 -0500

    Need to fix runtime error on midigen.py

--- genetic.py
+++ genetic.py
@@ -36,3 +36,88 @@ def single_point_crossover(a: Genome, b: Genome) -> Tuple[Genome, Genome]:
   # and vice versa
   p = randint(1, length - 1)
   return a[0:p] + b[p:], b[0:p] + a[p:]
+
+#mutates a genome at a random index if the generated num is less than the probability
+def mutation(genome: Genome, num: int = 1, probability: float = 0.5) -> Genome:
+  for _ in range(num):
+    index = randrange(len(genome))
+    genome[index] = genome[index] if random() > probability else abs(genome[index]-1)
+  return genome
+
+
+def population_fitness(population: Population, fitness_func: FitnessFunc) -> int:
+  return sum([fitness_func(genome) for genome in population])
+
+def selection_pair(population: Population, fitness_func: FitnessFunc) -> Population:
+    return sample(
+        population=generate_weighted_distribution(population, fitness_func),
+        k=2
+    )
+
+
+def generate_weighted_distribution(population: Population, fitness_func: FitnessFunc) -> Population:
+    result = []
+
+    for gene in population:
+        result += [gene] * int(fitness_func(gene)+1)
+
+    return result
+
+
+def sort_population(population: Population, fitness_func: FitnessFunc) -> Population:
+    return sorted(population, key=fitness_func, reverse=True)
+
+
+def genome_to_string(genome: Genome) -> str:
+    return "".join(map(str, genome))
+
+
+def print_stats(population: Population, generation_id: int, fitness_func: FitnessFunc):
+    print("GENERATION %02d" % generation_id)
+    print("=============")
+    print("Population: [%s]" % ", ".join([genome_to_string(gene) for gene in population]))
+    print("Avg. Fitness: %f" % (population_fitness(population, fitness_func) / len(population)))
+    sorted_population = sort_population(population, fitness_func)
+    print(
+        "Best: %s (%f)" % (genome_to_string(sorted_population[0]), fitness_func(sorted_population[0])))
+    print("Worst: %s (%f)" % (genome_to_string(sorted_population[-1]),
+                              fitness_func(sorted_population[-1])))
+    print("")
+
+    return sorted_population[0]
+
+
+def run_evolution(
+        populate_func: PopulateFunc,
+        fitness_func: FitnessFunc,
+        fitness_limit: int,
+        selection_func: SelectionFunc = selection_pair,
+        crossover_func: CrossoverFunc = single_point_crossover,
+        mutation_func: MutationFunc = mutation,
+        generation_limit: int = 100,
+        printer: Optional[PrinterFunc] = None) \
+        -> Tuple[Population, int]:
+    population = populate_func()
+
+    i = 0
+    for i in range(generation_limit):
+        population = sorted(population, key=lambda genome: fitness_func(genome), reverse=True)
+
+        if printer is not None:
+            printer(population, i, fitness_func)
+
+        if fitness_func(population[0]) >= fitness_limit:
+            break
+
+        next_generation = population[0:2]
+
+        for j in range(int(len(population) / 2) - 1):
+            parents = selection_func(population, fitness_func)
+            offspring_a, offspring_b = crossover_func(parents[0], parents[1])
+            offspring_a = mutation_func(offspring_a)
+            offspring_b = mutation_func(offspring_b)
+            next_generation += [offspring_a, offspring_b]
+
+        population = next_generation
+
+    return population, i
